b51c636c20efddeb7e84894d9194a32b8888f24b
Ritin-gitproject <Ritinproject@gmail.com>
2025-04-04 16:19:15 +0530

    Update custom-icon.js

--- src/js/custom-icon.js
+++ src/js/custom-icon.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const pathElements = svgDoc.querySelectorAll("path"); // Select all <path> elements
                 pathElements.forEach(path => {
                     path.classList.add("custom-path-class"); // Add class to each <path>
+                    path.style.strokeWidth = "2px"; // Set stroke-width directly via JS
                 });
             }
         });
