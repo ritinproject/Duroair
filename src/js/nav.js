e92914a26034e66932bcb6a043d0beae7ce1b2ab
Ritin-gitproject <Ritinproject@gmail.com>
2025-04-11 12:13:05 +0530

    Update nav.js

--- src/js/nav.js
+++ src/js/nav.js
@@ -43,10 +43,3 @@
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    let navLink = document.querySelector(".nav-military-and-government a.link");
-
-    if (navLink) {
-        navLink.textContent = navLink.textContent.replace(/\bGovernment And Military\b/g, "Military and Government");
-    }
-});
